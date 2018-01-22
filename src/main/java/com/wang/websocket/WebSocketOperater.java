8cf278b9319f765e263c0f71cca00714122e1f3d
SteakingCoder <1941499609@qq.com>
2018-01-22 22:43:16 +0800

    在websocket组件接收到消息传递时,就将聊天信息插入到数据库中(由于websocket中无法注入,undebug)

--- src/main/java/com/wang/websocket/WebSocketOperater.java
+++ src/main/java/com/wang/websocket/WebSocketOperater.java
@@ -2,12 +2,17 @@ package com.wang.websocket;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+
 import javax.annotation.Resource;
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+
 import org.springframework.stereotype.Service;
+
 import net.sf.json.JSONObject;
+
 import com.wang.model.UserList;
+import com.wang.service.ChatRecordService;
 import com.wang.service.UnreachHistoryService;
 
 
@@ -15,9 +20,11 @@ import com.wang.service.UnreachHistoryService;
 @Service
 public class WebSocketOperater {
 
-	//TODO 这个时候注入就不可以了
+	//TODO 这个时候注入就不可以了 ???
 	@Resource
 	private UnreachHistoryService unreachHistoryService;
+	@Resource
+	private ChatRecordService chatRecordService;
 	
 	private SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 	
@@ -31,12 +38,17 @@ public class WebSocketOperater {
 		Map<Session, String> userMap = UserList.getUserList();
 		Iterator iterator = userMap.keySet().iterator();
 		Session toSession = null;
+		//FUTURE
+		// 测试:非法用户名处理
+		
 		//用户在线
 		while(iterator.hasNext()){
 			toSession = (Session) iterator.next();
-			System.out.println(msgEntity[0]+" "+userMap.get(toSession)+" "+userMap.get(toSession).equalsIgnoreCase(msgEntity[0]));
+//			System.out.println(msgEntity[0]+" "+userMap.get(toSession)+" "+userMap.get(toSession).equalsIgnoreCase(msgEntity[0]));
 			if(userMap.get(toSession).equalsIgnoreCase(msgEntity[0])){
 				toSession.getBasicRemote().sendText(msgEntity[1]);
+				JSONObject msgJsonObject = JSONObject.fromObject(msgEntity[1]);
+				chatRecordService.addItem(msgJsonObject.getString("from"), msgEntity[0], msgJsonObject.getString("msg"), formatter.format(new Date()));
 				return;
 			}
 		}

0d8f4919c7ad023d4e803ad4f38ac50aeb2adf66
SteakingCoder <1941499609@qq.com>
2018-01-28 21:49:22 +0800

    更改Statment.sql,使之可以创建该项目所用的基本数据库存储

--- target/classes/SqlStatement.sql
+++ target/classes/SqlStatement.sql
@@ -1,48 +1,53 @@
-/*user table*/
-create table User(id varchar(20) primary key, imageUri varchar(200), sex varchar(10), age int, signature varchar(40), alive boolean default false);
-/*image table*/
+#创建数据表
+create database web_qq_db;
+use web_qq_db;
+create table user_set(id varchar(20) primary key, flag tinyint(1))ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table user(id varchar(30) primary key, imageUri varchar(200), sex varchar(10), age int, signature varchar(40), alive boolean default false);
 create table image(imageId varchar(40) primary key, imageUri varchar(300));
-/*chat log*/
-create table record_luo_wang(srcId varchar(20), dstId varchar(20), content varchar(512), timestamp Date,constraint foreign key (srcId) references User(id), foreign key (dstId) references User(id));
-/*friend table*/    
-create table friend_123 (friendId varchar(20) DEFAULT NULL, groupName varchar(200) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8    
-/*UserSet*/
-create table user_set(id varchar(20) primary key, flag boolean)  character set = utf8;
-/*history_123*/
-create table history_abc(friendId varchar(20), content varchar(512), timestamp datetime) character set = utf8;
-//插入未读消息
-insert into history_sss values('abc', 'content2&#xA9;&#x2693;&#x1F004', '2017-11-14 04:14:24');
 
-//插入图片icon
-insert into image valuse('defaultIcon', 'http://localhost:8080/SpringMVC/images/defaultIcon.jpg');
-insert into image valuse('defaultIcon1', 'http://localhost:8080/SpringMVC/images/defaultIcon1.jpg');
-insert into image valuse('defaultIcon2', 'http://localhost:8080/SpringMVC/images/defaultIcon2.jpg');
-insert into image valuse('defaultIcon3', 'http://localhost:8080/SpringMVC/images/defaultIcon3.jpg');
-
-//添加用户空间
-insert into user_set values('abc', 1);
-
-//添加用户
-insert into user values('abc', 'http://localhost:8080/SpringMVC/images/defaultIcon.jpg', 'woman', 11, 'signature');
+#插入图片icon
 
 
-//添加好友
-insert into friend_123 values('abc', 'http://localhost:8080/SpringMVC/images/defaultIcon\.jpg');   
-insert into friend_luo values('123', 'http://localhost:8080/SpringMVC/images/defaultIcon\.jpg');   
-create table record_123_abc(srcId varchar(20), dstId varchar(20), content varchar(512), constraint foreign key (srcId) references User(id), foreign key (dstId) references User(id));
+#添加用户空间
+insert into user_set values("abc", 0);
+insert into user_set values("123", 0);
+insert into user_set values("wang", 0);
+insert into user_set values("kun", 0);
+insert into user_set values("luo", 0);
+insert into user_set values("090201", 0);
+insert into user_set values("090202", 0);
+insert into user_set values("090203", 0);
+insert into user_set values("090204", 0);
+insert into user_set values("090205", 0);
+insert into user_set values("090206", 0);
+insert into user_set values("090207", 0);
+insert into user_set values("090208", 0);
+insert into user_set values("090209", 0);
+insert into user_set values("090211", 0);
+insert into user_set values("090212", 0);
+insert into user_set values("090213", 0);
+insert into user_set values("090214", 0);
+insert into user_set values("090215", 0);
+insert into user_set values("090216", 0);
+insert into user_set values("090217", 0);
+insert into user_set values("090218", 0);
+insert into user_set values("090219", 0);
+insert into user_set values("090221", 0);
+insert into user_set values("090222", 0);
+insert into user_set values("090223", 0);
+insert into user_set values("090224", 0);
+insert into user_set values("090225", 0);
+insert into user_set values("090226", 0);
+insert into user_set values("090227", 0);
+insert into user_set values("090228", 0);
+insert into user_set values("090229", 0);
+insert into user_set values("090231", 0);
+insert into user_set values("090232", 0);
+insert into user_set values("090233", 0);
+insert into user_set values("090234", 0);
+insert into user_set values("090235", 0);
+insert into user_set values("090236", 0);
+insert into user_set values("090237", 0);
+insert into user_set values("090238", 0);
+insert into user_set values("090239", 0);
 
-//添加未读历史消息
-
-alter table User drop foreign key user_ibfk_1;
-
-
-//插入聊天数据
-insert into record_abc_123 values("abc", "123", 'abc says', '2017-11-16 下午04:14:24');
-
-//聊天数据按时间逆序输出 优先输出最近的消息
-SELECT * FROM `record_123_abc` order by timestamp desc limit 0,2
-
-
-//修改用户头像
-
-UPDATE user SET imageUri='ssssssdefaultdddIcon.jpg' WHERE id = 'luo';
